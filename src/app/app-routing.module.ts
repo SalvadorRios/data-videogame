1d967837313f762b37f0231d8d7a97d49376827d
SalvadorRios <salvador.rios@interx.us>
2022-11-24 23:40:39 -0600

    modules info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { GameInfoComponent } from './game-info/game-info.component';
+import { QueryComponent } from './query/query.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:QueryComponent},
+  {path:'home', component: QueryComponent},
+  {path:'info', component: GameInfoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,17 @@ import { GraficasComponent } from './graficas/graficas.component';
 import { QueryComponent } from './query/query.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { GameInfoComponent } from './game-info/game-info.component';
+import { ErrorComponent } from './error/error.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     GraficasComponent,
-    QueryComponent
+    QueryComponent,
+    GameInfoComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
